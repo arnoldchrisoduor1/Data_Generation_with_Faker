84a7f3fa380e79afb72bf8e12bddc1389df70d2b
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-03-13 17:08:19 +0300

    generating unique values

--- 9-unique_values.py
+++ 9-unique_values.py
@@ -2,7 +2,11 @@ from faker import Faker
 
 fake = Faker()
 
-names = [fake.unique.first_name() for i in range(500)]
+names = [fake.unique.first_name() for i in range(10)]
 assert len(set(names)) == len(names)
 
 print(names)
+
+for i in range(3):
+    fake.unique.boolean()
+    
